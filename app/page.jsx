b92f369e9897dacfa1f97967ae88551b7fb9f705
HZJ <7775230914@qq.com>
2026-05-12 14:33:08 +0800

    Add large cat option to pet type select

--- app/page.jsx
+++ app/page.jsx
@@ -176,6 +176,7 @@ export default function Home() {
                       <option value="large-dog">大型犬</option>
                       <option value="medium-dog">中型犬</option>
                       <option value="small-dog">小型犬</option>
+                      <option value="large-cat">大型猫</option>
                       <option value="cat">猫咪</option>
                       <option value="reptile">爬行类动物</option>
                       <option value="guoguo-jiao">锅锅叫</option>
